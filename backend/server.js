8fce67e60510d0f83f3babf80a65149c29597318
David Byrd <dbyrd@depparts.com>
2019-05-02 12:32:00 -0400

    server

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use(bodyParser.json());
 app.use(logger("dev"));
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'));
+  app.use(express.static('client/src'));
 };
 
 app.get("/", (req, res) => {
--- server.js
+++ server.js
@@ -7,8 +7,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "build")));
 
+
+
 app.get("/", (req, res) => {
-   res.sendFile(path.join(__dirname + "/build/index.html"));
+   res.sendFile(path.join(__dirname + "./client/src/index.html"));
 });
 
 app.listen(PORT, () => {
