9ed65b9430bd18398d4afe48c5fcf3fa58672acf
David Byrd <dbyrd@depparts.com>
2019-05-02 12:42:35 -0400

    server

--- client/server.js
+++ client/server.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const path = require("path");
+// const express = require("express");
+// const path = require("path");
 
-const app = express();
+// const app = express();
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, "build")));
+// app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/", (req, res) => {
-   res.sendFile(path.join(__dirname + "./src/index.html"));
-});
+// app.get("/", (req, res) => {
+//    res.sendFile(path.join(__dirname + "./src/index.html"));
+// });
 
-app.listen(PORT, () => {
-   console.log("Server Started on Port ", PORT);
-});
+// app.listen(PORT, () => {
+//    console.log("Server Started on Port ", PORT);
+// });
